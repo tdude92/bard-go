10b2cabd4ef7db0be440b58b84b6b9a9d8dbe4b4
Trevor Du <t34du@uwaterloo.ca>
2023-09-16 13:25:32 -0400

    basic image upload following the FLASK website tutorial

--- server/main.py
+++ server/main.py
@@ -1,13 +1,21 @@
 import os
 import psycopg2
-from dotenv import load_dotenv
-from flask import Flask, jsonify, request
+# from dotenv import load_dotenv
+from flask import Flask, flash, redirect, url_for, send_from_directory, jsonify, request
+#from werkzeug import secure_filename
 
-load_dotenv( )
+# load_dotenv( )
+UPLOAD_FOLDER = os.path.relpath("/temp/")
+ALLOWED_EXTENSIONS = {"jpg", "jpeg"}
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 # url = os.getenv("DATABASE_URL")
 # connection = psycopg2.connect(url)
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.get('/')
 def home():
@@ -17,11 +25,33 @@ def home():
 def emotions():
     return "emotions"
 
-@app.post('/api/image')
-def image():
-    data = request.get_json()
-    image = data["image"]
-    return {"type": "SUCCESS", "response": image}
+@app.route('/api/image', methods=['GET','POST'])
+def upload_file():
+    if request.method == "POST":
+        # Check if the request has the file part
+        if 'file' not in request.files:
+            return redirect(request.url)
+        file = request.files["file"]
+        # If the user sent no file, the browser also submits an empty part with no filename
+        if file.filename == '':
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = file.filename
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file', filename=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+
+@app.route('/api/image/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__=="__main__":
     app.run(debug=True, port=8080)
