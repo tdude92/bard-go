3f507bdad425581b4ad0302eb1d4796a3dc9fef0
Ingrid Qin <iqin@uwaterloo.ca>
2023-09-16 13:28:23 -0400

    images

--- server/main.py
+++ server/main.py
@@ -2,12 +2,23 @@ import os
 import psycopg2
 from dotenv import load_dotenv
 from flask import Flask, jsonify, request
+from PIL import Image
 
-load_dotenv( )
+load_dotenv()
 
 app = Flask(__name__)
 # url = os.getenv("DATABASE_URL")
 # connection = psycopg2.connect(url)
+
+def delete_img(img_path):
+    print(img_path)
+    os.remove(os.path.relpath(img_path))
+        
+def find_emotion(img_path): # temp/image_name
+    # puts image into neural network to determine emotion
+    delete_img(img_path)
+    return "EMOTIONAL DAMAGE"
+
 
 @app.get('/')
 def home():
@@ -15,13 +26,16 @@ def home():
 
 @app.get('/api/emotions')
 def emotions():
-    return "emotions"
+    return find_emotion(img_path="temp/sample1.jpg")
 
 @app.post('/api/image')
 def image():
+    # upload 
     data = request.get_json()
     image = data["image"]
     return {"type": "SUCCESS", "response": image}
+
+# upload into temp folder then delete from temp folder
 
 if __name__=="__main__":
     app.run(debug=True, port=8080)
